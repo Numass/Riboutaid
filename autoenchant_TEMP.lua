d0e28573516c41a4ab4048944902a1aa812f0c19
Numass <numaalt1@gmail.com>
2025-08-11 19:23:50 +0200

    Update autoenchant_TEMP.lua

--- autoenchant_TEMP.lua
+++ autoenchant_TEMP.lua
@@ -390,7 +390,7 @@ local function autoEnchantLoop()
                     
                     if success then
                         print("[Auto Enchant] Enchanting successful, waiting for completion...")
-                        wait(0.1) -- Wait for enchanting to complete
+                        wait(3) -- Wait for enchanting to complete
                         
                         -- Check if pets got desired enchants, if not, add them back to queue
                         for _, petData in ipairs(batchPets) do
@@ -432,7 +432,7 @@ local function autoEnchantLoop()
                 end
             end
             
-            wait(0.3)
+            wait(1)
         end
     end)
 end
